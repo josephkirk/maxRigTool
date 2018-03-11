0be823658542b26dcb304fa79127ce72efb38c19
Phi Hung Nguyen <josephkirk.art@gmail.com>
2018-03-11 19:48:06 +0700

    first commit

---
+++ script_install.bat
@@ -0,0 +1,6 @@
+
+xcopy "%~dp0javaScripts" "C:\Program Files\Autodesk\3ds Max 2016\Scripts\javaScripts\" /e /y /s
+xcopy "%~dp0macroScripts" "C:\Program Files\Autodesk\3ds Max 2016\MacroScripts\" /e /y /s
+
+
+pause
